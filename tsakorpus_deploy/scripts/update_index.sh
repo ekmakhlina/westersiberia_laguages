1b77311bc7235346b70b7b24fe8bfa5e93f763f2
Ekaterina Makhlina <ekaterinamakhlina@gmail.com>
2020-08-12 01:15:23 +0300

    Add tsakorpus configs and tsakorpus update script

---
+++ tsakorpus_deploy/scripts/update_index.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SOURCE_ROOT_DIR="$( cd $(dirname $0) && pwd -P)/.."
+
+TSAKORPUS_DIR=${TSAKORPUS_DIR:-"$HOME/tsakorpus"}
+CONFIGS_DIR=${CONFIGS_DIR:-"$SOURCE_ROOT_DIR/configs"}
+
+(cd $CONFIGS_DIR; find . -type f | 
+    while read filename; do
+        echo "$filename"
+        dir="$TSAKORPUS_DIR/$( dirname $filename )"
+        mkdir -p "$dir" && cp "$CONFIGS_DIR/$filename" "$dir"
+    done)
+
+( cd "$TSAKORPUS_DIR/indexator" && python indexator.py )
+
+
