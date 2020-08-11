e91eba9489bbfe9a70dda8f5dfbda2bc20b0a64c
Ekaterina Makhlina <ekaterinamakhlina@gmail.com>
2020-08-12 01:47:15 +0300

    Add version save

--- tsakorpus_deploy/scripts/update_index.sh
+++ tsakorpus_deploy/scripts/update_index.sh
@@ -5,6 +5,14 @@ SOURCE_ROOT_DIR="$( cd $(dirname $0) && pwd -P)/.."
 TSAKORPUS_DIR=${TSAKORPUS_DIR:-"$HOME/tsakorpus"}
 CONFIGS_DIR=${CONFIGS_DIR:-"$SOURCE_ROOT_DIR/configs"}
 
+version_file() {
+    echo "$HOME/.westernsiberia_languages_version"
+}
+
+update() {
+    test "$( cat $( version_file ) )" != "$( git rev-parse HEAD )"
+}
+
 (cd $CONFIGS_DIR; find . -type f | 
     while read filename; do
         echo "$filename"
@@ -12,6 +20,11 @@ CONFIGS_DIR=${CONFIGS_DIR:-"$SOURCE_ROOT_DIR/configs"}
         mkdir -p "$dir" && cp "$CONFIGS_DIR/$filename" "$dir"
     done)
 
-( cd "$TSAKORPUS_DIR/indexator" && python indexator.py )
+if test update; then
+    ( cd "$TSAKORPUS_DIR/indexator" && python indexator.py &&
+        git rev-parse HEAD > $( version_file ) )
+else
+    echo "Up to date"
+fi
 
 
