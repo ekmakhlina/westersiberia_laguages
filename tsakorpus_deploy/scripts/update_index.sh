5e912cb81b8a9614ea1ae8642bb88933040ff0a2
k1nkreet <polyakovskiy.ilya@gmail.com>
2020-08-12 10:26:22 +0300

    Remove saving git-rev logic from update script

--- tsakorpus_deploy/scripts/update_index.sh
+++ tsakorpus_deploy/scripts/update_index.sh
@@ -5,14 +5,6 @@ SOURCE_ROOT_DIR="$( cd $(dirname $0) && pwd -P)/.."
 TSAKORPUS_DIR=${TSAKORPUS_DIR:-"$HOME/tsakorpus"}
 CONFIGS_DIR=${CONFIGS_DIR:-"$SOURCE_ROOT_DIR/configs"}
 
-version_file() {
-    echo "$HOME/.westernsiberia_languages_version"
-}
-
-update() {
-    test "$( cat $( version_file ) )" != "$( git rev-parse HEAD )"
-}
-
 (cd $CONFIGS_DIR; find . -type f | 
     while read filename; do
         echo "$filename"
@@ -20,11 +12,6 @@ update() {
         mkdir -p "$dir" && cp "$CONFIGS_DIR/$filename" "$dir"
     done)
 
-if test update; then
-    ( cd "$TSAKORPUS_DIR/indexator" && python indexator.py &&
-        git rev-parse HEAD > $( version_file ) )
-else
-    echo "Up to date"
-fi
+( cd "$TSAKORPUS_DIR/indexator" && python indexator.py )
 
 
